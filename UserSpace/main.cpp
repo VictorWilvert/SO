f71149f6915cf6b174a55b75f5844b198c365b71
Victor Wilvert Antunes <victorwilvertantunes@gmail.com>
2018-11-18 16:40:50 -0200

    Update main.cpp

--- UserSpace/main.cpp
+++ UserSpace/main.cpp
@@ -5,7 +5,11 @@
 #include <iostream>
 
 int main(void) {
-	RTTTLParser parser;
+	RTTTLParser parser("rtttl_examples/Zombie.txt");
 	BuzzerPlayer buzzer;
+	buzzer.tempo(parser.tempo());
+	while (1) {
+		buzzer.playMusic(parser.notes());
+	}
 	return 0;
 }
