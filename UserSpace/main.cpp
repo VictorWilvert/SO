ab1e162dbd3bf4e13b271547239fe1eea2ff3c18
Victor Wilvert Antunes <victorwilvertantunes@gmail.com>
2018-11-18 17:39:33 -0200

    Update main.cpp

--- UserSpace/main.cpp
+++ UserSpace/main.cpp
@@ -5,11 +5,24 @@
 #include <iostream>
 
 int main(void) {
-	RTTTLParser parser("examples/halloween.txt");
+	RTTTLParser parser;
 	BuzzerPlayer buzzer;
-	buzzer.tempo(parser.tempo());
+	std::string music;
+	unsigned int count;
+	std::cout << "Tocador de musicas no formato RTTTL" << std::endl
+	std::cout << "As musicas de exemplo se encontram no diretorio 'examples'" << std::endl;
+	std::cout << std::endl;
 	while (1) {
-		buzzer.playMusic(parser.notes());
+		
+		std::cout << "Qual musica se deseja ouvir: ";
+		std::cin >> music;
+		parser(music);
+		std::cout << "Quantas vezes: ";
+		std::cin >> count;
+		buzzer.tempo(parser.tempo());
+		for(auto i = 0;i < count;++i) {
+			buzzer.playMusic(parser.notes());
+		}
 	}
 	return 0;
 }
