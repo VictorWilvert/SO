2738940a68f624845fb60fd43c72b39508ccc09f
Victor Wilvert Antunes <victorwilvertantunes@gmail.com>
2018-11-18 16:56:38 -0200

    Update main.cpp

--- UserSpace/main.cpp
+++ UserSpace/main.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 
 int main(void) {
-	RTTTLParser parser("rtttl_examples/Ac\ Dc\ -\ Back\ In\ Black.txt");
+	RTTTLParser parser("rtttl_examples/AC\ DC\ -\ Highway\ to\ Hell.txt");
 	BuzzerPlayer buzzer;
 	buzzer.tempo(parser.tempo());
 	while (1) {
